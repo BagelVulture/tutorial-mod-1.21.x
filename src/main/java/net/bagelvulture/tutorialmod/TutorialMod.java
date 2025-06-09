6c260adf6d21b7ef7745f4ed6379cc17e75668c9
BagelVulture <ethanlhuot@gmail.com>
2025-06-09 17:22:31 -0400

    added item "bevultrium", but no resources for it

--- src/main/java/net/bagelvulture/tutorialmod/TutorialMod.java
+++ src/main/java/net/bagelvulture/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.bagelvulture.tutorialmod;
 
+import net.bagelvulture.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
