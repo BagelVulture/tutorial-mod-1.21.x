610bb4e2c5a1943898cca09dc30e60779e39a1a8
BagelVulture <ethanlhuot@gmail.com>
2025-06-26 17:12:18 -0400

    added cool sound effects

--- src/main/java/net/bagelvulture/tutorialmod/TutorialMod.java
+++ src/main/java/net/bagelvulture/tutorialmod/TutorialMod.java
@@ -6,8 +6,11 @@ import net.bagelvulture.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.bagelvulture.tutorialmod.item.ModItemGroups;
 import net.bagelvulture.tutorialmod.util.HammerUsageEvent;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +31,15 @@ public class TutorialMod implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModItems.FERTILE_BEVULTRIUM, 200);
 
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if (!world.isClient && !player.isCreative()) {
+				String selected = null;
+				String[] options = {"POW!", "BANG!", "BASH!", "KAPOW!", "KABLAMO!", "WHAM!", "BOOM!"};
+				selected = options[new java.util.Random().nextInt(options.length)];
+				player.sendMessage(Text.literal(selected));
+			}
+			return ActionResult.PASS;
+		});
 	}
 }
