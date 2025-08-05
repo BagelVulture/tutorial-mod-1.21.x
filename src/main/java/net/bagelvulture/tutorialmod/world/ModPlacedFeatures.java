7d347797e983daa55ed833558b648c803cde8e1b
BagelVulture <ethanlhuot@gmail.com>
2025-08-05 17:39:36 -0400

    added tree generation

--- src/main/java/net/bagelvulture/tutorialmod/world/ModPlacedFeatures.java
+++ src/main/java/net/bagelvulture/tutorialmod/world/ModPlacedFeatures.java
@@ -1,16 +1,14 @@
 package net.bagelvulture.tutorialmod.world;
 
 import net.bagelvulture.tutorialmod.TutorialMod;
+//import net.bagelvulture.tutorialmod.block.ModBlocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 //import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.feature.*;
 //import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 
@@ -20,6 +18,8 @@ public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> BEVULTRIUM_ORE_PLACED_KEY = registerKey("bevultrium_ore_placed");
     public static final RegistryKey<PlacedFeature> NETHER_BEVULTRIUM_ORE_PLACED_KEY = registerKey("nether_bevultrium_ore_placed");
     public static final RegistryKey<PlacedFeature> END_BEVULTRIUM_ORE_PLACED_KEY = registerKey("end_bevultrium_ore_placed");
+
+    public static final RegistryKey<PlacedFeature> DISEASED_PLACED_KEY = registerKey("diseased_placed");
 
     public static void bootstrap(Registerable<PlacedFeature> context) {
         var configuredFeatures = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
@@ -35,6 +35,11 @@ public class ModPlacedFeatures {
         //register(context, END_BEVULTRIUM_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_BEVULTRIUM_ORE_KEY),
         //        ModOrePlacement.modifiersWithCount(14,
         //                HeightRangePlacementModifier.uniform(YOffset.fixed(-64), YOffset.fixed(256))));
+
+                                //this gives an error, but it wouldn't have done anything anyway so... oh well!
+        //register(context, DISEASED_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.DISEASED_KEY),
+        //        VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+        //                PlacedFeatures.createCountExtraModifier(0, 0.2f, 1), ModBlocks.DISEASED_SAPLING));
     }
 
     public static RegistryKey<PlacedFeature> registerKey(String name) {
