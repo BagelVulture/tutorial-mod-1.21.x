350ac5ef962e092a95976b906d26be24302608d6
BagelVulture <ethanlhuot@gmail.com>
2025-09-03 12:29:01 -0400

    added a boss bar for the boss bar

--- src/main/java/net/bagelvulture/tutorialmod/entity/custom/BossBarEntity.java
+++ src/main/java/net/bagelvulture/tutorialmod/entity/custom/BossBarEntity.java
@@ -6,17 +6,24 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.ServerBossBar;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class BossBarEntity extends AnimalEntity {
+
+    private final ServerBossBar bossBar = new ServerBossBar(Text.literal("Boss Bar For The Boss Bar Boss"),
+            BossBar.Color.GREEN, BossBar.Style.NOTCHED_10);
 
     public BossBarEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
@@ -56,5 +63,24 @@ public class BossBarEntity extends AnimalEntity {
     @Override
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return ModEntities.BOSSBAR.create(world);
+    }
+
+    /* BOSS BAR */
+    @Override
+    public void onStartedTrackingBy(ServerPlayerEntity player) {
+        super.onStartedTrackingBy(player);
+        this.bossBar.addPlayer(player);
+    }
+
+    @Override
+    public void onStoppedTrackingBy(ServerPlayerEntity player) {
+        super.onStoppedTrackingBy(player);
+        this.bossBar.removePlayer(player);
+    }
+
+    @Override
+    protected void mobTick() {
+        super.mobTick();
+        this.bossBar.setPercent(this.getHealth() / this.getMaxHealth());
     }
 }
