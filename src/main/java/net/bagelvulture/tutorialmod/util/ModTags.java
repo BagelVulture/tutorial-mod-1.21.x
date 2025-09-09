864c9b3c9f58b73143cda18a46330a31d49ebce0
BagelVulture <ethanlhuot@gmail.com>
2025-09-09 10:26:04 -0400

    tagified pinking recipes

--- src/main/java/net/bagelvulture/tutorialmod/util/ModTags.java
+++ src/main/java/net/bagelvulture/tutorialmod/util/ModTags.java
@@ -23,6 +23,21 @@ public class ModTags {
     public static class Items {
         public static final TagKey<Item> DISEASED_LOGS = createTag("diseased_logs");
 
+        //for pink
+        public static final TagKey<Item> WOOLS = createTag("wools");
+        public static final TagKey<Item> CARPETS = createTag("carpets");
+        public static final TagKey<Item> BEDS = createTag("beds");
+        public static final TagKey<Item> TERRACOTTAS = createTag("terracottas");
+        public static final TagKey<Item> GLAZED_TERRACOTTAS = createTag("glazed_terracottas");
+        public static final TagKey<Item> CONCRETES = createTag("concretes");
+        public static final TagKey<Item> STAINED_GLASS = createTag("stained_glass");
+        public static final TagKey<Item> STAINED_GLASS_PANES = createTag("stained_glass_panes");
+        public static final TagKey<Item> SHULKER_BOXES = createTag("shulker_boxes");
+        public static final TagKey<Item> CONCRETE_POWDERS = createTag("concrete_powders");
+        public static final TagKey<Item> CANDLES = createTag("candles");
+        public static final TagKey<Item> BANNERS = createTag("banners");
+        public static final TagKey<Item> DYES = createTag("dyes");
+
         private static TagKey<Item> createTag(String path) {
             return TagKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, path));
         }
